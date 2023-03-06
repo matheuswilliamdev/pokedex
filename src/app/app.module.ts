8fc714d7a5ef33543120835d732d88e65243f2db
matheuswilliam01 <matheuswilliam2002@hotmail.com>
2023-03-06 20:37:16 -0300

    Creation of main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FotosPokemonComponent } from './components/fotos-pokemon/fotos-pokemon.component';
+import { CardPokemonComponent } from './components/card-pokemon/card-pokemon.component';
+import { DetalheComponent } from './components/detalhe/detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FotosPokemonComponent,
+    CardPokemonComponent,
+    DetalheComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/fotos-pokemon/fotos-pokemon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fotos-pokemon',
+  templateUrl: './fotos-pokemon.component.html',
+  styleUrls: ['./fotos-pokemon.component.scss']
+})
+export class FotosPokemonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
